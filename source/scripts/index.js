e41de2b6a2f2534816b693cb14c16db842e811aa
Александр <aleksandr_abrame@mail.ru>
2025-12-19 21:31:45 +0300

    update

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.main-nav');
+const menuButton = document.querySelector('.main-nav__toggle');
+
+nav.classList.add('main-nav--closed');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.toggle('main-nav--closed');
+  nav.classList.toggle('main-nav--opened');
+});
